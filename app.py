1059156d9fb007e38437ab1e3fe5caad9aa3b33f
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2023-04-02 17:25:36 +0200

    improved readability

--- app.py
+++ app.py
@@ -6,29 +6,29 @@ app = Flask(__name__)
 global person # current person
 
 # welcome page
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 # get user inputs
-@app.route("/start/", methods=["POST", "GET"])
+@app.route('/start/', methods=['POST', 'GET'])
 def inputs():
-    if request.method == "POST":
+    if request.method == 'POST':
         global person # current person
-        person = meeter.Person(str(request.form["name_input"]), str(request.form["phone_input"]), str(request.form["degree_input"]), request.form.getlist('interests_input')) # assign inputs
+        person = meeter.Person(str(request.form['name_input']), str(request.form['phone_input']), str(request.form['degree_input']), request.form.getlist('interests_input')) # assign inputs
         meeter.people.append(person) # add to database
-        return redirect("/matches/")
+        return redirect('/matches/')
     else:
-        return render_template("input.html")
+        return render_template('input.html')
 
 # view matches
-@app.route("/matches/")
+@app.route('/matches/')
 def matches():
         global person # current person
         matches:list[meeter.Person] = []
         matches = person.matches.get_matches()
-        if matches != []: return render_template("matches.html", matches=matches)
-        else: return render_template("matches.html", notice='No matches yet!')
+        if matches != []: return render_template('matches.html', matches=matches)
+        else: return render_template('matches.html', notice='No matches yet!')
 
 if __name__ == '__main__':
     app.run(debug=True)
